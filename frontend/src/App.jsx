6f2013ebf8b2f10cf7b7155b5b827554ab849e94
Saulo Mendes <sauloml@gmail.com>
2024-04-17 01:29:58 -0300

    tweak research card appearence

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import ResearchList from "./components/research-list";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Scientify</h1>
--- frontend/src/components/research-list/index.jsx
+++ frontend/src/components/research-list/index.jsx
@@ -4,70 +4,66 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import { Box } from "@mui/material";
+import { researchData } from "../../data/researchData";
 
 export default function ResearchList() {
-  const researchData = [
-    {
-      id: "1",
-      name: "Research 1",
-      description: "This is my research",
-      image_url: "https://picsum.photos/200/200",
-      authors: [
-        {
-          name: "Marcelo",
-          url: "www.lattes.com.br/marcelo",
-        },
-      ],
-      cost: 1000000,
-      invested_amount: 633001,
-      read_amount: 300,
-    },
-    {
-      id: "2",
-      name: "Research 2",
-      description: "This is my research",
-      image_url: "https://picsum.photos/200/200",
-      authors: [
-        {
-          name: "Marcelo",
-          url: "www.lattes.com.br/marcelo",
-        },
-      ],
-      cost: 1000000,
-      invested_amount: 633001,
-      read_amount: 300,
-    },
-    {
-      id: "3",
-      name: "Research 3",
-      description: "This is my research",
-      image_url: "https://picsum.photos/200/200",
-      authors: [
-        {
-          name: "Marcelo",
-          url: "www.lattes.com.br/marcelo",
-        },
-      ],
-      cost: 1000000,
-      invested_amount: 633001,
-      read_amount: 300,
-    },
-  ];
-
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: 15 }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 15,
+        textAlign: "left",
+        flexWrap: "wrap",
+        flexGrow: 1,
+      }}
+    >
       {researchData.map((research) => (
-        <Card sx={{ display: "flex" }} key={research.id}>
-          <CardMedia component="img" height="140" image={research.image_url} />
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            maxWidth: 320,
+          }}
+          key={research.id}
+        >
+          <CardMedia
+            component="img"
+            sx={{ width: "100%", height: 120 }}
+            image={research.image_url}
+          />
+          <Box sx={{}}>
+            <CardContent sx={{}}>
+              <Typography gutterBottom variant="h6" component="div">
                 {research.name}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
+              <Typography gutterBottom variant="h7" component="div">
+                Author(s):{" "}
+                {research.authors.map((author, i) => [
+                  i > 0 && ", ",
+                  author.name,
+                ])}
+              </Typography>
+              <Typography gutterBottom variant="body2" color="text.secondary">
                 {research.description}
               </Typography>
+
+              <Divider sx={{ margin: 0 }} variant="inset" />
+
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  marginTop: 2,
+                }}
+              >
+                <Typography>${research.cost}</Typography>
+                <Typography>${research.investment}</Typography>
+                <Typography>${research.earnings}</Typography>
+              </Box>
             </CardContent>
             <CardActions>
               <Button size="small">Read Article</Button>
